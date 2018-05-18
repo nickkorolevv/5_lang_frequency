1a7cba61798aa2dcb1ab449eeceeae13fd8a5448
nickkorolevv <37769528+nickkorolevv@users.noreply.github.com>
2018-05-18 19:37:54 +0400

    new

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,39 @@
+import re
+import collections
+import sys
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, "r", encoding="windows-1251") as file_handler:
+        text = file_handler.read()
+        return text
 
 
 def get_most_frequent_words(text):
-    pass
+    match_obj = re.compile("([a-zA-Zа-яА-ЯёЁ']+)")
+    text_data = match_obj.findall(text)
+    word_list = []
+    for word in text_data:
+        word_list.append(word.lower())
+
+    freq_dict = collections.Counter(word_list)
+    return freq_dict
 
 
-if __name__ == '__main__':
-    pass
+def print_top_word(sentence, freq_dict):
+    print(sentence)
+    counter = 1
+    for i in freq_dict.most_common(10):
+        print(str(counter)+')', i[0], "встречается", i[1], "раз(а)")
+        counter += 1
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        filepath = format(sys.argv[1])
+    else:
+        exit("Файл не выбран, пожалуйста, выберите файл")
+
+    text = load_data(filepath)
+    freq_dict = get_most_frequent_words(text)
+    print_top_word("Самые популярные слова: ", freq_dict)
