36e8c4260a5a2ec6df7c7ed1dbe832eba1e1ea68
nickkorolevv <37769528+nickkorolevv@users.noreply.github.com>
2018-05-20 13:55:45 +0400

    добавлены исключения

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,24 +1,28 @@
 import re
 import collections
 import sys
+import os
 
 
 def load_data(filepath):
     with open(filepath, "r", encoding="windows-1251") as file_handler:
-        text = file_handler.read()
-        return text
+        try:
+            text = file_handler.read()
+            return text
+        except ValueError:
+            return None
 
 
-def get_most_frequent_words(text, COUNT):
+def get_most_frequent_words(text, max_count):
     text_list = re.findall("([a-zA-Zа-яА-ЯёЁ']+)", text.lower())
-    word_frequency = collections.Counter(text_list).most_common(COUNT)
+    word_frequency = collections.Counter(text_list).most_common(max_count)
     return word_frequency
 
 
-def print_top_word(sentence, word_frequency, COUNT):
+def print_top_word(sentence, words):
     print(sentence)
-    for count, word in enumerate(word_frequency):
-        print((count+1), "- {} встречается {} раз(а)".format(word[0], word[1]))
+    for index, (word, count) in enumerate(words, 1):
+        print(index, "- {} встречается {} раз(а)".format(word, count))
 
 
 if __name__ == "__main__":
@@ -26,7 +30,13 @@ if __name__ == "__main__":
         filepath = sys.argv[1]
     else:
         exit("Файл не выбран, пожалуйста, выберите файл")
-    text = load_data(filepath)
-    COUNT = 10
-    word_frequency = get_most_frequent_words(text, COUNT)
-    print_top_word("Самые популярные слова: ", word_frequency, COUNT)
+    if os.path.exists(filepath):
+        text = load_data(filepath)
+        if text is not None:
+            max_count = 10
+            words = get_most_frequent_words(text, max_count)
+            print_top_word("Самые популярные слова: ", words)
+        else:
+            exit("Файл не поддерживается")
+    else:
+        exit("Файла нет в директории")
